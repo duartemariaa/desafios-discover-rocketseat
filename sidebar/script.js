9a650b3a5d4b3976357f42798853a05179642625
Maria Duarte <mariaduarte061@hotmail.com>
2023-07-11 11:52:53 -0300

    updates

--- sidebar/script.js
+++ sidebar/script.js
@@ -2,21 +2,12 @@ let sidebarOpen = true;
 
 function toggleSideBar(){
     const sidebar = document.querySelector(".sidebar");
-    const menuText = document.querySelector(".sidebar-menu-text");
-    /*const menuText = sidebar.querySelector(".sidebar-menu-text");*/
-    /*const menuText = sidebar.querySelectorAll(".sidebar-menu-text");*/
+    const menuTexts = document.querySelectorAll(".hide-menu-close");
+        for(let i=0; i< menuTexts.length; i++){
+            menuTexts[i].style.display = sidebarOpen
+            ? "none" : "inline";
+        }
     sidebar.style.width = sidebarOpen ? "60px" : "240px";
-    sidebar.style.width = sidebarOpen ? menuText.style.visibility = "hidden" : menuText.style.visibility = "visible";
+    
     sidebarOpen = !sidebarOpen;
 }
-
-
-
-/*document.querySelector(".sidebar-header-button").addEventListener('click', function(){
-    const sidebarButton = document.querySelector(".sidebar");
-    if(sidebarButton.style.width === '240px'){
-        sidebarButton.style.width = '50px';
-    }else{
-        sidebarButton.style.width = '240px';
-    }
-})*/

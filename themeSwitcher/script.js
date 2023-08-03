5cce0daf0d15cac855900817c6add6e1c866ae4e
Maria Duarte <mariaduarte061@hotmail.com>
2023-08-03 11:25:26 -0300

    struture change and project finalized

--- themeSwitcher/script.js
+++ themeSwitcher/script.js
@@ -9,9 +9,16 @@ function getThemePreference(){
 function applyThemePreference(){
     const bodyColor = document.body;
     const storedTheme = getThemePreference();
+    const slider = document.querySelector('.slider');
 
     if(storedTheme){
         bodyColor.style.backgroundColor = storedTheme;
+        if(storedTheme === '#292C35'){
+            slider.style.transform = 'translateX(63px)';
+        }
+        else{
+            slider.style.transform = 'tranlateX(0)';
+        }
     }else{
         bodyColor.style.backgroundColor = '#F1F1F1';    
     }
@@ -19,14 +26,17 @@ function applyThemePreference(){
 
 function toggleBackground(){
     const bodyColor = document.body;
-    const atualColor = bodyColor.style.backgroundColor; 
+    const atualColor = bodyColor.style.backgroundColor;
+    const slider = document.querySelector('.slider');
 
     if(atualColor === 'rgb(241, 241, 241)'){
         bodyColor.style.backgroundColor = '#292C35';   
-        setThemePreference('#292C35')
+        setThemePreference('#292C35');
+        slider.style.transform = 'translateX(63px)';
     }else{
         bodyColor.style.backgroundColor = '#F1F1F1';    
-        setThemePreference('#F1F1F1')
+        setThemePreference('#F1F1F1');
+        slider.style.transform = 'translateX(0)';
     }
 }
 
